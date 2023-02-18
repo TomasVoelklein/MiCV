cddb08aa7eaa406a76ce6426a2060b72a717c0f3
tomas <tomas_voelklein@hotmail.com>
2023-02-17 23:11:54 -0300

    hice el hero con mi nombre df#	new file:   .vscode/launch.json df#	new file:   css/index.css hd#	new file:   img/wewe.jpg hice el hero con mi nombre#	new file:   index.html df#	new file:   js/funciones.js

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+console.log ("hola mundo")
+
+function llevameAbajo()   {
+    console.log ("capturamos el evento click para despues llevarnos a la pagina principal");
+    document.getElementById("demo").innerHTML = "evento que me lleva a la pag principal";
+    };
+function ac1pt() {
+    document.body.style.backgroundColor = "green"
+}
