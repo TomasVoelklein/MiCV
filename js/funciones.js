defb7d1f80b4bf532df07a97c3e2f9b0461f4dc3
tomas <tomas_voelklein@hotmail.com>
2023-02-23 00:35:43 -0300

    cree la pagina de contacto y agregue funcion a los botones

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,5 @@
 console.log ("hola mundo")
 
-function llevameAbajo()   {
-    console.log ("capturamos el evento click para despues llevarnos a la pagina principal");
-    document.getElementById("demo").innerHTML = "evento que me lleva a la pag principal";
-    };
-function ac1pt() {
-    document.body.style.backgroundColor = "green"
-}
+document.getElementsByClassName('bx bxl-gmail').addEventListener("click", function() {
+    window.location.href = "mailto:destinatario@ejemplo.com";
+});
