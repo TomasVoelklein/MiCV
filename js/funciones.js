02465bb3097d38e678cd8c37b12a9304c3bc6f04
tomas <tomas_voelklein@hotmail.com>
2023-02-25 16:47:23 -0300

    cambios generales

--- js/funciones.js
+++
@@ -1,5 +0,0 @@
-console.log ("hola mundo")
-
-document.getElementsByClassName('bx bxl-gmail').addEventListener("click", function() {
-    window.location.href = "mailto:destinatario@ejemplo.com";
-});
